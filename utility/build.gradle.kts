62d78ff087be0395f6a2c0d5eaec8207d7bce930
allensun <allen.sun@gotinder.com>
2025-02-12 00:16:41 -0800

    add utility module

---
+++ utility/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    kotlin("jvm") version "2.0.20"
+}
+
+group = "org.kalmanfilter"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
