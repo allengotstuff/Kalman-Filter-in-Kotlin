986fee1da99b93c6e2b6ea1c7cf5b601e0829d8d
allensun <allen.sun@gotinder.com>
2025-02-13 13:05:43 -0800

    refactor data generation for 3d test

--- utility/src/main/kotlin/org/kalmanfilter/utility/GenerateApacheTestDataSet.kt
+++ utility/src/main/kotlin/org/kalmanfilter/utility/GenerateApacheTestDataSet.kt
@@ -7,16 +7,19 @@ import org.apache.commons.math3.linear.RealMatrix
 import org.apache.commons.math3.linear.RealVector
 
 class GeneratedApacheDataSet(
-    // Inputs for the process and measurement models.
+    //State
     private val stateTransition: RealMatrix,             // State transition matrix (A)
-    private val control: RealMatrix,                     // Control input matrix (B); pass null if unused
-    private val processNoise: RealMatrix,                // Process noise covariance matrix (Q)
     private val initialStateEstimate: RealVector,        // Initial state estimate
     private val initialErrorCovariance: RealMatrix,      // Initial error covariance matrix (P0)
-    private val measMatrix: RealMatrix,                  // Measurement matrix (H)
-    private val measNoise: RealMatrix,                   // Measurement noise covariance matrix (R)
+    //Process noise
+    private val processNoise: RealMatrix,                // Process noise covariance matrix (Q)
+    //Measurement
+    private val measurementMatrix: RealMatrix,           // Measurement matrix (H)
+    private val measurementCovariance: RealMatrix,       // Measurement noise covariance matrix (R)
     private val measurementVectors: List<RealVector>,    // List of measurement vectors for correct() steps
-    private val measurementCovariance: List<RealMatrix>  // List of measurement error
+    //Control
+    private val control: RealMatrix,                     // Control input matrix (B); pass null if unused
+    private val controlVectors: List<RealVector>         // List of control vector for each prediction
 ) {
 
     // Lists to store the state estimations and error covariance matrices from each iteration.
@@ -28,8 +31,7 @@ class GeneratedApacheDataSet(
 
     init {
 
-        check(measurementCovariance.size == measurementVectors.size){ "Measurement vectors must have same size with covariance" }
-        check(allMatricesIdentical(measurementCovariance)){"Apache kalman filter doesn't support measurementCovariance change per iteration: $measurementCovariance"}
+        check(controlVectors.size == measurementVectors.size){"Control vectors must have same size with measure vectors"}
 
         val processModel = DefaultProcessModel(
             stateTransition,
@@ -38,7 +40,7 @@ class GeneratedApacheDataSet(
             initialStateEstimate,
             initialErrorCovariance
         )
-        val measurementModel = DefaultMeasurementModel(measMatrix, measNoise)
+        val measurementModel = DefaultMeasurementModel(measurementMatrix, measurementCovariance)
         kalmanFilter = KalmanFilter(processModel, measurementModel)
     }
 
@@ -50,18 +52,12 @@ class GeneratedApacheDataSet(
      * error covariance matrix.
      */
     fun generate() {
-        measurementVectors.forEach { measurement ->
-            kalmanFilter.predict()
+        measurementVectors.zip(controlVectors).forEach { (measurement, control) ->
+            kalmanFilter.predict(control)
             kalmanFilter.correct(measurement)
             // Store copies of the current state estimation vector and error covariance matrix.
             stateEstimations.add(kalmanFilter.stateEstimationVector.copy())
             errorCovariances.add(kalmanFilter.errorCovarianceMatrix.copy())
         }
-    }
-
-    private fun allMatricesIdentical(matrices: List<RealMatrix>): Boolean {
-        if (matrices.isEmpty()) return true
-        val reference = matrices.first()
-        return matrices.all { it == reference }
     }
 }
