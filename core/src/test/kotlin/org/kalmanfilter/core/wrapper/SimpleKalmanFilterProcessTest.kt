a3eb1fe9a7663fd1886136d9d80130daa1549f79
allensun <allen.sun@gotinder.com>
2025-02-16 12:21:08 -0800

    Add test for update steps

--- core/src/test/kotlin/org/kalmanfilter/core/wrapper/SimpleKalmanFilterProcessTest.kt
+++ core/src/test/kotlin/org/kalmanfilter/core/wrapper/SimpleKalmanFilterProcessTest.kt
@@ -7,6 +7,7 @@ import org.apache.commons.math3.linear.ArrayRealVector
 import org.apache.commons.math3.linear.MatrixUtils
 import org.junit.jupiter.api.BeforeEach
 import org.kalmanfilter.core.KalmanFilterCore
+import org.kalmanfilter.core.wrapper.SimpleKalmanFilterProcess.DebugStats
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.`when`
 import org.mockito.kotlin.any
@@ -36,22 +37,6 @@ class SimpleKalmanFilterProcessTest {
 
         kfMock = mock(KalmanFilterCore::class.java)
 
-        `when`(
-            kfMock.update(
-                any(),
-                any(),
-                any(),
-                any(),
-                any()
-            )
-        ).thenAnswer {
-            KalmanFilterCore.KalmanUpdateResult(
-                x = ArrayRealVector(2),
-                p = MatrixUtils.createRealIdentityMatrix(2),
-                k = MatrixUtils.createRealIdentityMatrix(2)
-            )
-        }
-
         //Given
         simpleKF = SimpleKalmanFilterProcess(
             debugEnabled = true,
@@ -65,7 +50,7 @@ class SimpleKalmanFilterProcessTest {
 
     @Test
     fun `given predict once, validate params set correctly with expected value update`() {
-        
+
         val newState = ArrayRealVector(doubleArrayOf(7.0, 8.0, 9.0))
         val newCovariance = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(51.0)
 
@@ -100,12 +85,12 @@ class SimpleKalmanFilterProcessTest {
 
         `when`(
             kfMock.predict(
-               any(),
-               any(),
-               any(),
-               any(),
-               any(),
-               any()
+                any(),
+                any(),
+                any(),
+                any(),
+                any(),
+                any()
             )
         ).thenAnswer {
             Pair(newState, newCovariance)
@@ -150,7 +135,141 @@ class SimpleKalmanFilterProcessTest {
     }
 
     @Test
-    fun `given update, validate params set correctly with expected value update`() {
+    fun `given update once, validate params set correctly with expected value update`() {
+
+        // Given
+        val newMeasureVector = ArrayRealVector(doubleArrayOf(13.0, 14.0, 15.0))
+        val newMeasureCovariance = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(71.0)
+        val newMeasureMatrix = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(81.0)
+
+        val newState = ArrayRealVector(doubleArrayOf(16.0, 17.0, 18.0))
+        val newCovariance = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(91.0)
+        val newK = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(101.0)
+
+        `when`(
+            kfMock.update(
+                x = initialState,
+                y = newMeasureVector,
+                p = initialCovariance,
+                h = newMeasureMatrix,
+                r = newMeasureCovariance
+            )
+        ).thenAnswer {
+            KalmanFilterCore.KalmanUpdateResult(
+                x = newState,
+                p = newCovariance,
+                k = newK
+            )
+        }
+
+        // When
+        simpleKF.update(
+            y = newMeasureVector,
+            r = newMeasureCovariance,
+            measureMatrix = newMeasureMatrix
+        )
+
+        // Then
+        assertEquals(getState(simpleKF), newState)
+        assertEquals(getCovariance(simpleKF), newCovariance)
+    }
+
+    @Test
+    fun `given update multiple, debugResult should have expected dataset`() {
+
+        // Given first invocation
+        `when`(
+            kfMock.update(
+                any(),
+                any(),
+                any(),
+                any(),
+                any()
+            )
+        ).thenAnswer {
+            KalmanFilterCore.KalmanUpdateResult(
+                x = ArrayRealVector(doubleArrayOf(19.0, 20.0, 21.0)),
+                p = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(111.0),
+                k = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(121.0)
+            )
+        }
+
+        simpleKF.update(
+            y = ArrayRealVector(doubleArrayOf(22.0, 23.0, 24.0)),
+            r = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(131.0),
+            measureMatrix = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(141.0)
+        )
+
+        // Given second invocation
+        `when`(
+            kfMock.update(
+                any(),
+                any(),
+                any(),
+                any(),
+                any()
+            )
+        ).thenAnswer {
+            KalmanFilterCore.KalmanUpdateResult(
+                x = ArrayRealVector(doubleArrayOf(25.0, 26.0, 27.0)),
+                p = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(151.0),
+                k = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(161.0)
+            )
+        }
+
+        simpleKF.update(
+            y = ArrayRealVector(doubleArrayOf(28.0, 29.0, 30.0)),
+            r = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(171.0),
+            measureMatrix = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(181.0)
+        )
+
+        // Given third invocation
+        `when`(
+            kfMock.update(
+                any(),
+                any(),
+                any(),
+                any(),
+                any()
+            )
+        ).thenAnswer {
+            KalmanFilterCore.KalmanUpdateResult(
+                x = ArrayRealVector(doubleArrayOf(31.0, 32.0, 33.0)),
+                p = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(191.0),
+                k = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(201.0)
+            )
+        }
+
+        simpleKF.update(
+            y = ArrayRealVector(doubleArrayOf(34.0, 35.0, 36.0)),
+            r = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(211.0),
+            measureMatrix = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(221.0)
+        )
+
+        // Then
+        assertEquals(
+            simpleKF.debug(),
+            listOf(
+                DebugStats(
+                    x = ArrayRealVector(doubleArrayOf(19.0, 20.0, 21.0)),
+                    p = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(111.0),
+                    k = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(121.0),
+                    y = ArrayRealVector(doubleArrayOf(22.0, 23.0, 24.0))
+                ),
+                DebugStats(
+                    x = ArrayRealVector(doubleArrayOf(25.0, 26.0, 27.0)),
+                    p = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(151.0),
+                    k = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(161.0),
+                    y = ArrayRealVector(doubleArrayOf(28.0, 29.0, 30.0))
+                ),
+                DebugStats(
+                    x = ArrayRealVector(doubleArrayOf(31.0, 32.0, 33.0)),
+                    p = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(191.0),
+                    k = MatrixUtils.createRealIdentityMatrix(3).scalarMultiply(201.0),
+                    y = ArrayRealVector(doubleArrayOf(34.0, 35.0, 36.0))
+                )
+            )
+        )
 
     }
 
